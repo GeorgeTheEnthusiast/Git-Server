3afd1117787bac964eaadae45f589fb1721d063e
Pedzel <warmemotions@gmail.com>
2018-11-09 17:14:55 +0100

    copying the same logic to get action

--- api/controllers/changesController.js
+++ api/controllers/changesController.js
@@ -27,9 +27,7 @@ exports.send = function(req, res) {
         changeType = 'Delete';
         break;
     };
-    console.log(x.Path);
     x.Path = x.Path.replace(/\\/g, "\\\\");
-    console.log(x.Path);
 
     console.log(`Inserting ${x.FileName}...`);
 
@@ -51,5 +49,45 @@ exports.send = function(req, res) {
 exports.get = function(req, res) {
     console.log('GET');
     
+    var mysql = require('mysql');
+
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+   
+    connection.connect();
+    var groupId = Math.random().toString(36).substr(2, 9);
+  
+    req.body.GitChanges.forEach(x => {
+      var changeType = '';
+  
+      switch(x.ChangeType){
+          case 0:
+          changeType = 'Unset';
+          break;
+          case 1:
+          changeType = 'Add';
+          break;
+          case 2:
+          changeType = 'Modify';
+          break;
+          case 3:
+          changeType = 'Delete';
+          break;
+      };
+      x.Path = x.Path.replace(/\\/g, "\\\\");
+  
+      console.log(`Inserting ${x.FileName}...`);
+  
+      var sql = `INSERT INTO changes (Path, ChangeType, FileName, Content, Created, GroupId) VALUES ('${x.Path}', '${changeType}', '${x.FileName}', '${x.Base64Content}', '${new Date().toJSON()}', '${groupId}')`;
+  
+      connection.commit(sql, function(err, rows, fields) {
+      
+        if (err) throw err;
+      
+        
+      });
+    });
+  
+    connection.end();
+
     res.json('OK - get');
 };
